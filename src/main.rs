08a845937fb704224e02ef8c651d0463e61c414c
Seongji Park <scott0202@kaist.ac.kr>
2022-09-24 03:23:19 +0900

    블록 굴리기 구현

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
     cmp::{max, min},
+    f32::consts::PI,
     mem::swap,
 };
 
-use bevy::{input::keyboard, prelude::*, scene::SceneBundle, DefaultPlugins};
+use bevy::{prelude::*, DefaultPlugins};
+
+const BLOCK_MOVE_TIME: f32 = 0.5;
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
 enum GameState {
@@ -26,24 +29,26 @@ struct Game {
     board: Vec<Vec<Option<Block>>>,
     x: i32,
     z: i32,
+    move_timer: Timer,
 }
 
 impl Game {
-    pub fn move_block(&mut self, dx: i32, dz: i32, mut transforms: Query<&mut Transform>) {
-        if self.x + dx < 0 || self.x + dx > 3 || self.z + dz < 0 || self.z + dz > 3 {
+    pub fn move_block(&mut self, dx: i32, dz: i32) {
+        if self.x + dx < 0
+            || self.x + dx > 3
+            || self.z + dz < 0
+            || self.z + dz > 3
+            || !self.move_timer.finished()
+        {
             return;
         }
 
         // translate block
         let (x0, z0) = (self.x as usize, self.z as usize);
         let (x1, z1) = ((self.x + dx) as usize, (self.z + dz) as usize);
-        let block = self.board[x1][z1].as_ref().unwrap();
-        let mut asdf = transforms.get_mut(block.entity).unwrap();
-        asdf.translation += Vec3 {
-            x: -dx as f32,
-            y: 0.0,
-            z: -dz as f32,
-        };
+        let block = self.board[x1][z1].as_mut().unwrap();
+        block.moving = Some((dx, dz));
+        self.move_timer = Timer::from_seconds(BLOCK_MOVE_TIME, false);
 
         // swap self.board[x0][z0] and self.board[x1][z1]
         if x0 == x1 {
@@ -61,6 +66,7 @@ impl Game {
 struct Block {
     entity: Entity,
     number: i32,
+    moving: Option<(i32, i32)>,
 }
 
 fn setup(
@@ -81,8 +87,8 @@ fn setup(
                             mesh: cube_mesh.clone(),
                             material: materials.add(StandardMaterial {
                                 base_color: Color::Rgba {
-                                    red: z as f32 / 4.0,
-                                    green: x as f32 / 4.0,
+                                    red: z as f32 / 3.0,
+                                    green: x as f32 / 3.0,
                                     blue: 0.5,
                                     alpha: 0.5,
                                 },
@@ -95,6 +101,7 @@ fn setup(
                         })
                         .id(),
                     number: (x * 4 + z + 1) as i32 % 16,
+                    moving: None,
                 })
             } else {
                 None
@@ -103,6 +110,7 @@ fn setup(
     }
     game.x = 3;
     game.z = 3;
+    game.move_timer = Timer::from_seconds(0.1, false);
 
     // light
     const HALF_SIZE: f32 = 10.0;
@@ -144,25 +152,65 @@ fn setup(
 }
 
 fn update_block(
+    time: Res<Time>,
     mut game: ResMut<Game>,
     keyboard_input: Res<Input<KeyCode>>,
     mut transforms: Query<&mut Transform>,
 ) {
-    // for arr in game.board.iter() {
-    //     for block in arr {
-    //         transforms
-    //             .get_mut(block.entity)
-    //             .unwrap()
-    //             .rotate_y(block.number as f32 / 100.0);
-    //     }
-    // }
-    if keyboard_input.just_pressed(KeyCode::Up) {
-        game.move_block(0, -1, transforms);
-    } else if keyboard_input.just_pressed(KeyCode::Down) {
-        game.move_block(0, 1, transforms);
-    } else if keyboard_input.just_pressed(KeyCode::Left) {
-        game.move_block(-1, 0, transforms);
-    } else if keyboard_input.just_pressed(KeyCode::Right) {
-        game.move_block(1, 0, transforms);
+    let timer_finished = game.move_timer.tick(time.delta()).just_finished();
+    let elapsed_secs = game.move_timer.elapsed_secs();
+    for arr in game.board.iter_mut() {
+        for elem in arr {
+            if let Some(block) = elem {
+                if let Some((dx, dz)) = block.moving {
+                    let mut transform = transforms.get_mut(block.entity).unwrap();
+                    let move_dist =
+                        (2.0 as f32).sqrt() * 0.25 * PI * time.delta_seconds() / BLOCK_MOVE_TIME;
+                    let move_angle = PI * 0.5 * (0.5 - elapsed_secs / BLOCK_MOVE_TIME);
+                    transform.translation += Vec3 {
+                        x: -dx as f32 * move_dist * move_angle.cos(),
+                        y: move_dist * move_angle.sin(),
+                        z: -dz as f32 * move_dist * move_angle.cos(),
+                    };
+                    transform
+                        .rotate_x(-dz as f32 * PI * 0.5 * time.delta_seconds() / BLOCK_MOVE_TIME);
+                    transform
+                        .rotate_z(dx as f32 * PI * 0.5 * time.delta_seconds() / BLOCK_MOVE_TIME);
+                    if timer_finished {
+                        let rotation_round = |i: f32| {
+                            let abs = i.abs();
+                            let sgn = i.signum();
+                            if abs < 0.25 {
+                                0.0
+                            } else if abs < 0.6 {
+                                0.5 * sgn
+                            } else if abs < 0.85 {
+                                (2.0 as f32).sqrt() * 0.5 * sgn
+                            } else {
+                                1.0 * sgn
+                            }
+                        };
+                        transform.translation.x = transform.translation.x.round();
+                        transform.translation.y = transform.translation.y.round();
+                        transform.translation.z = transform.translation.z.round();
+                        transform.rotation.x = rotation_round(transform.rotation.x);
+                        transform.rotation.y = rotation_round(transform.rotation.y);
+                        transform.rotation.z = rotation_round(transform.rotation.z);
+                        transform.rotation.w = rotation_round(transform.rotation.w);
+                        block.moving = None;
+                    }
+                }
+            }
+        }
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        game.move_block(0, 1);
+    } else if keyboard_input.pressed(KeyCode::Down) {
+        game.move_block(0, -1);
+    } else if keyboard_input.pressed(KeyCode::Left) {
+        game.move_block(1, 0);
+    } else if keyboard_input.pressed(KeyCode::Right) {
+        game.move_block(-1, 0);
     }
 }
