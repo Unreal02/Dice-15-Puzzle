11fc8054a2b2818666ccb741fefe6e6ae2389d0a
Unreal <scott0202@kaist.ac.kr>
2022-11-04 23:26:44 +0900

    make reset/shuffle button

---
+++ src/game_ui.rs
@@ -0,0 +1,122 @@
+use bevy::prelude::*;
+
+const NORMAL_COLOR: Color = Color::rgb(1.0, 1.0, 1.0);
+const PRESSED_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
+
+#[derive(Component)]
+enum MyButtons {
+    Reset,
+    Shuffle,
+}
+
+pub struct GameUIPlugin;
+
+impl Plugin for GameUIPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_buttons)
+            .add_system(button_system);
+    }
+}
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &MyButtons),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, buttons) in &mut interaction_query {
+        match *interaction {
+            Interaction::Clicked => {
+                match buttons {
+                    MyButtons::Reset => println!("reset"),
+                    MyButtons::Shuffle => println!("shuffle"),
+                }
+                *color = PRESSED_COLOR.into();
+            }
+            _ => *color = NORMAL_COLOR.into(),
+        }
+    }
+}
+
+fn setup_buttons(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/Quicksand-Bold.ttf");
+
+    commands
+        .spawn()
+        .insert_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .insert_bundle(SpatialBundle::default())
+        .insert(Name::new("GAME_UI"))
+        .with_children(|parent| {
+            // reset button
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: Style {
+                        align_self: AlignSelf::Center,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        size: Size::new(Val::Px(200.0), Val::Px(100.0)),
+                        position_type: PositionType::Absolute,
+                        position: UiRect {
+                            right: Val::Px(50.0),
+                            bottom: Val::Px(50.0),
+                            ..default()
+                        },
+                        margin: UiRect::all(Val::Auto),
+                        ..default()
+                    },
+                    color: NORMAL_COLOR.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        "Reset",
+                        TextStyle {
+                            font: font.clone(),
+                            font_size: 48.0,
+                            color: Color::BLACK,
+                        },
+                    ));
+                })
+                .insert(MyButtons::Reset);
+
+            // shuffle button
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: Style {
+                        align_self: AlignSelf::Center,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        size: Size::new(Val::Px(200.0), Val::Px(100.0)),
+                        position_type: PositionType::Absolute,
+                        position: UiRect {
+                            right: Val::Px(50.0),
+                            bottom: Val::Px(200.0),
+                            ..default()
+                        },
+                        margin: UiRect::all(Val::Auto),
+                        ..default()
+                    },
+                    color: NORMAL_COLOR.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        "Shuffle",
+                        TextStyle {
+                            font: font.clone(),
+                            font_size: 48.0,
+                            color: Color::BLACK,
+                        },
+                    ));
+                })
+                .insert(MyButtons::Shuffle);
+        });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod block;
 mod game;
+mod game_ui;
 mod player;
 
 use bevy::{prelude::*, DefaultPlugins};
 use bevy_inspector_egui::{RegisterInspectable, WorldInspectorPlugin};
 use game::{GamePlugin, GameState};
+use game_ui::GameUIPlugin;
 use player::PlayerPlugin;
 
 fn main() {
@@ -15,5 +17,6 @@ fn main() {
         .register_inspectable::<GameState>()
         .add_plugin(PlayerPlugin)
         .add_plugin(GamePlugin)
+        .add_plugin(GameUIPlugin)
         .run();
 }
