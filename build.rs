0b72154e6090618c7e1bf94f75005a47f09e4e4d
Unreal <scott0202@kaist.ac.kr>
2022-11-16 02:39:24 +0900

    change block texture

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
+use std::iter::Sum;
+
+use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
-use image::{ImageBuffer, RgbImage};
+use image::{imageops, ImageBuffer, Rgb, RgbImage};
 
 fn main() {
     // generate image textures from
@@ -23,18 +26,35 @@ fn main() {
         .unwrap()
         .to_rgb8();
 
+    let mut img_goal_small = imageops::resize(&img_goal, 512, 512, FilterType::Nearest);
+    for x in 0..512 {
+        for y in 0..512 {
+            let mut pixel = *img_goal_small.get_pixel(x, y);
+            for i in 0..3 {
+                pixel.0[i] = ((pixel.0[i] as u32 + 128) * 2 / 3) as u8;
+            }
+            img_goal_small.put_pixel(x, y, pixel);
+        }
+    }
+
     let mut img: RgbImage = ImageBuffer::new(1024, 768);
+    for x in 0..1024 {
+        for y in 0..768 {
+            img.put_pixel(x, y, Rgb::from([255, 255, 255]));
+        }
+    }
+
     for x in 0..256 {
         for y in 0..256 {
-            let img_side_pixel = *img_side.get_pixel(x, y);
+            let side_pixel = *img_side.get_pixel(x, y);
             // up
-            img.put_pixel(511 - x, 255 - y, img_side_pixel);
+            img.put_pixel(511 - x, 255 - y, side_pixel);
             // down
-            img.put_pixel(256 + x, 512 + y, img_side_pixel);
+            img.put_pixel(256 + x, 512 + y, side_pixel);
             // left
-            img.put_pixel(256 - y, 256 + x, img_side_pixel);
+            img.put_pixel(256 - y, 256 + x, side_pixel);
             // right
-            img.put_pixel(512 + y, 511 - x, img_side_pixel);
+            img.put_pixel(512 + y, 511 - x, side_pixel);
             // back
             img.put_pixel(768 + x, 256 + y, *img_back.get_pixel(x, y));
         }
@@ -43,12 +63,25 @@ fn main() {
     for i in 0..15 {
         for x in 0..256 {
             for y in 0..256 {
+                let goal_pixel = *img_goal.get_pixel(x + (i % 4) * 256, y + (i / 4) * 256);
                 // goal
-                img.put_pixel(
-                    x + 256,
-                    y + 256,
-                    *img_goal.get_pixel(x + (i % 4) * 256, y + (i / 4) * 256),
-                );
+                img.put_pixel(x + 256, y + 256, goal_pixel);
+            }
+        }
+        for x in 0..128 {
+            for y in 0..128 {
+                let goal_small_pixel =
+                    *img_goal_small.get_pixel(x + (i % 4) * 128, y + (i / 4) * 128);
+                // up
+                img.put_pixel(511 - (64 + x), 255 - (64 + y), goal_small_pixel);
+                // down
+                img.put_pixel(256 + (64 + x), 512 + (64 + y), goal_small_pixel);
+                // left
+                img.put_pixel(256 - (64 + y), 256 + (64 + x), goal_small_pixel);
+                // right
+                img.put_pixel(512 + (64 + y), 511 - (64 + x), goal_small_pixel);
+                // back
+                img.put_pixel(768 + (64 + x), 256 + (64 + y), goal_small_pixel);
             }
         }
         img.save(format!("assets/images/image{}.png", i + 1))
--- src/block.rs
+++ src/block.rs
@@ -118,7 +118,6 @@ pub fn spawn_meshes(
             if x != 3 || z != 3 {
                 let texture =
                     asset_server.load(format!("images/image{}.png", x + z * 4 + 1).as_str());
-                let lightness = (x + 1) as f32 / 5.0;
                 mesh_entities.insert(
                     (x, z),
                     commands
@@ -127,10 +126,10 @@ pub fn spawn_meshes(
                             material: materials.add(StandardMaterial {
                                 base_color_texture: Some(texture.clone()),
                                 base_color: match z {
-                                    0 => Color::hsl(0.0, 1.0, lightness),
-                                    1 => Color::hsl(60.0, 1.0, lightness),
-                                    2 => Color::hsl(120.0, 1.0, lightness),
-                                    3 => Color::hsl(240.0, 1.0, lightness),
+                                    0 => Color::hsl(0.0, 1.0, 0.6),
+                                    1 => Color::hsl(60.0, 1.0, 0.6),
+                                    2 => Color::hsl(120.0, 1.0, 0.6),
+                                    3 => Color::hsl(240.0, 1.0, 0.6),
                                     _ => Color::BLACK,
                                 },
                                 ..default()
