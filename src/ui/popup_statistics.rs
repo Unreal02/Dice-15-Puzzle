a9110714e48c0d55dc35323081bc6138bc1a6263
Unreal <scott0202@kaist.ac.kr>
2023-01-11 16:13:49 +0900

    add details(scroll bar) in popup_statistics

--- src/ui/popup_statistics.rs
+++ src/ui/popup_statistics.rs
@@ -1,7 +1,17 @@
+use bevy::input::mouse::{MouseScrollUnit, MouseWheel};
+
 use crate::{
     duration_to_string::duration_to_string, player::PlayerState,
     statistics_manager::StatisticsManager, ui::*,
 };
+
+const SCROLL_BAR_MAX_ITEMS: usize = 11;
+const SCROLL_SPEED: f32 = 1.0;
+
+#[derive(Component, Default)]
+pub struct ScrollBar {
+    position: f32,
+}
 
 pub fn spawn_popup_statistics(
     mut commands: Commands,
@@ -43,7 +53,7 @@ pub fn spawn_popup_statistics(
                         left: Val::Px(25.0),
                         ..default()
                     },
-                    format!("Solves: {}", statistics_manager.solves()),
+                    format!("Solves\n{}", statistics_manager.solves()),
                     font.clone(),
                 );
 
@@ -52,12 +62,12 @@ pub fn spawn_popup_statistics(
                     spawn_text(
                         parent,
                         UiRect {
-                            top: Val::Px(120.0),
+                            top: Val::Px(180.0),
                             left: Val::Px(25.0),
                             ..default()
                         },
                         format!(
-                            "Average: {}",
+                            "Average\n{}",
                             duration_to_string(statistics_manager.average())
                         ),
                         font.clone(),
@@ -67,11 +77,11 @@ pub fn spawn_popup_statistics(
                     spawn_text(
                         parent,
                         UiRect {
-                            top: Val::Px(160.0),
+                            top: Val::Px(280.0),
                             left: Val::Px(25.0),
                             ..default()
                         },
-                        format!("Best: {}", duration_to_string(statistics_manager.best())),
+                        format!("Best\n{}", duration_to_string(statistics_manager.best())),
                         font.clone(),
                     );
 
@@ -79,13 +89,61 @@ pub fn spawn_popup_statistics(
                     spawn_text(
                         parent,
                         UiRect {
-                            top: Val::Px(200.0),
+                            top: Val::Px(380.0),
                             left: Val::Px(25.0),
                             ..default()
                         },
-                        format!("Worst: {}", duration_to_string(statistics_manager.worst())),
+                        format!("Worst\n{}", duration_to_string(statistics_manager.worst())),
                         font.clone(),
                     );
+
+                    // details (text)
+                    spawn_text(
+                        parent,
+                        UiRect {
+                            top: Val::Px(80.0),
+                            left: Val::Px(325.0),
+                            ..default()
+                        },
+                        "Details".to_string(),
+                        font.clone(),
+                    );
+
+                    // scroll bar background
+                    parent
+                        .spawn((
+                            NodeBundle {
+                                style: Style {
+                                    position_type: PositionType::Absolute,
+                                    align_items: AlignItems::Center,
+                                    justify_content: JustifyContent::Center,
+                                    size: Size::new(Val::Px(275.0), Val::Px(450.0)),
+                                    position: UiRect {
+                                        right: Val::Px(25.0),
+                                        bottom: Val::Px(25.0),
+                                        ..default()
+                                    },
+                                    ..default()
+                                },
+                                background_color: Color::rgb(0.2, 0.2, 0.2).into(),
+                                ..default()
+                            },
+                            ScrollBar::default(),
+                        ))
+                        .with_children(|parent| {
+                            for i in 0..SCROLL_BAR_MAX_ITEMS {
+                                spawn_text(
+                                    parent,
+                                    UiRect {
+                                        top: Val::Px(5.0 + 40.0 * i as f32),
+                                        left: Val::Px(25.0),
+                                        ..default()
+                                    },
+                                    "".to_string(),
+                                    font.clone(),
+                                );
+                            }
+                        });
                 }
             });
         });
@@ -94,10 +152,46 @@ pub fn spawn_popup_statistics(
 pub fn popup_system_statistics(
     mut player_state: ResMut<State<PlayerState>>,
     keyboard_input: Res<Input<KeyCode>>,
+    mut scroll_events: EventReader<MouseWheel>,
+    mut scroll_bar_query: Query<(&mut ScrollBar, &Children)>,
+    statistics_manager_query: Query<&StatisticsManager>,
+    mut text_query: Query<&mut Text>,
 ) {
+    let statistics_manager = statistics_manager_query.single();
+
     // press Esc: popup close
     if keyboard_input.just_pressed(KeyCode::Escape) {
         let _ = player_state.pop();
+    }
+
+    // scroll bar
+    if let Ok((mut scroll_bar, children)) = scroll_bar_query.get_single_mut() {
+        for scroll_event in scroll_events.iter() {
+            let dy = match scroll_event.unit {
+                MouseScrollUnit::Line => scroll_event.y,
+                MouseScrollUnit::Pixel => scroll_event.y / 20.0,
+            };
+            scroll_bar.position -= dy * SCROLL_SPEED;
+        }
+
+        scroll_bar.position = scroll_bar
+            .position
+            .clamp(0.0, (statistics_manager.solves() as f32 - 11.0).max(0.0));
+
+        let start_position = scroll_bar.position as usize;
+        for i in 0..children.len() {
+            let index = start_position + i;
+            text_query.get_mut(children[i]).unwrap().sections[0].value =
+                if index < statistics_manager.solves() {
+                    format!(
+                        "{}. {}",
+                        index + 1,
+                        duration_to_string(statistics_manager.records[index])
+                    )
+                } else {
+                    "".to_string()
+                };
+        }
     }
 }
 
