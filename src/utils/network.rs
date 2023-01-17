aa0b5f561b27d09931db0927971cc70ecad7ab75
Unreal <scott0202@kaist.ac.kr>
2023-01-18 02:38:01 +0900

    use tungstenite native-tls

--- src/utils/network.rs
+++ src/utils/network.rs
@@ -1,13 +1,13 @@
 use tungstenite::{self, connect, Message};
 use url::Url;
 
-const SERVER_ADDR: &str = "ws://dice15puzzle-server.haje.org:1515";
+const SERVER_ADDR: &str = "wss://dice15puzzle-server.haje.org";
 
 pub struct Network;
 
 impl Network {
     pub fn request() {
-        println!("{:?}", Url::parse(SERVER_ADDR));
+        println!("{:?}", Url::parse(SERVER_ADDR).unwrap());
         let (mut socket, _) = connect(Url::parse(SERVER_ADDR).unwrap()).expect("connect fail");
 
         let request = "get daily puzzle\n";
