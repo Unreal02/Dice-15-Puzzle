d88811f92100073dcc6303504acc693dc0dc458b
Unreal <scott0202@kaist.ac.kr>
2023-01-20 03:49:48 +0900

    http 서버 통신 테스트

--- src/network/network.rs
+++ src/network/network.rs
@@ -1,40 +1,31 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::AsyncComputeTaskPool};
 use chrono::NaiveDate;
-use wasm_sockets::{EventClient, Message};
 
 use crate::network::RequestType;
 
-const SERVER_ADDR: &str = "wss://dice15puzzle-server.haje.org"; // actual server
+const SERVER_ADDR: &str = "https://dice15puzzle-server.haje.org"; // actual server
 
-// const SERVER_ADDR: &str = "ws://localhost"; // local server
+// const SERVER_ADDR: &str = "http://localhost:1515"; // local server
 
 pub struct Network;
 
 impl Network {
     fn request(req: RequestType) {
-        let mut client = EventClient::new(SERVER_ADDR).unwrap();
-
-        client.set_on_error(Some(Box::new(|error| {
-            info!("Error\n{:#?}", error);
-        })));
-
-        client.set_on_connection(Some(Box::new(move |client: &EventClient| {
-            info!("Connected\n{:#?}", client.status);
-            info!("Sending message...");
-            let message = serde_json::to_string(&req).unwrap();
-            client.send_string(&message).unwrap();
-        })));
-
-        client.set_on_close(Some(Box::new(|_evt| {
-            info!("Connection closed");
-        })));
-
-        client.set_on_message(Some(Box::new(|client: &EventClient, message: Message| {
-            info!("New Message: {:#?}", message);
-        })));
+        let thread_pool = AsyncComputeTaskPool::get();
+        thread_pool.spawn(async move {
+            let res = reqwest::get(SERVER_ADDR)
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+            info!("{}", res);
+        });
+        info!("asdf");
     }
 
     pub fn get_daily_puzzle(date: NaiveDate) {
         Self::request(RequestType::GetDailyPuzzle(date));
+        info!("qwerqwer");
     }
 }
